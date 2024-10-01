25b1750418ca98259ab1f5cf2387a039a7e789bd
germer <lzh1098435117@gmail.com>
2024-10-01 09:09:15 +0530

    strings solve

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let trimstr = input.trim();
+    trimstr.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let pushstr = input.to_string();
+    pushstr + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let replacestr = input.replace("cars", "balloons");
+    replacestr.to_string()
+
 }
 
 #[cfg(test)]
